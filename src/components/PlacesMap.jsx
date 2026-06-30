ca827e51ac7a24df2b7e59fc751cb59e8eebaece
pnguyen174 <96810934+pnguyen174@users.noreply.github.com>
2026-06-29 23:03:32 -0700

    finished map

--- src/components/PlacesMap.jsx
+++ src/components/PlacesMap.jsx
@@ -14,6 +14,18 @@ import seattle from "../assets/seattle.JPG";
 import vegas from "../assets/vegas.jpeg";
 import haidilao from "../assets/haidilao.jpeg";
 import sd from "../assets/sd.jpeg";
+import disney from "../assets/disney.jpeg";
+import fusiondelight from "../assets/fusiondelight.jpeg";
+import mdo from "../assets/mdo.JPG";
+import morrobay from "../assets/morrobay.jpeg";
+import mumuhotpot from "../assets/mumuhotpot.JPG";
+import reseda from "../assets/reseda.JPG";
+import sb from "../assets/sb.jpeg";
+import sensorio from "../assets/sensorio.jpeg";
+import sf from "../assets/sf.jpeg";
+import sizzlingogi from "../assets/sizzlingogi.JPG";
+import sj from "../assets/sj.JPG";
+import waterlantern from "../assets/waterlantern.jpeg";
 
 const PLACES = [
   {
@@ -98,6 +110,114 @@ const PLACES = [
     lat: 35.179105,
     lng: -120.733817,
     photo: beach,
+  },
+  {
+    id: "11",
+    name: "First DISNEYLAND TRIPPP!",
+    caption:
+      "It made me so happy to go to Disneyland with you! Thank you for going with me and fulfilling my childhood dreams of going with my future wifey.",
+    lat: 33.805917,
+    lng: -117.91965,
+    photo: disney,
+  },
+  {
+    id: "12",
+    name: "Fusion Delight",
+    caption:
+      "Fusion delight so yummmmy. Thank you for putting me on Chinese foooood hehehe. I love bok choyyyyy",
+    lat: 37.68938,
+    lng: -122.138222,
+    photo: fusiondelight,
+  },
+  {
+    id: "13",
+    name: "Montana De Oro",
+    caption:
+      "I can't wait to start going to MDO with you again. It is so pretty there. I've never been there and you showed it to me and its so beautiful like youuu.",
+    lat: 35.296538,
+    lng: -120.868926,
+    photo: mdo,
+  },
+  {
+    id: "14",
+    name: "Morro Bay Ramen shop",
+    caption:
+      "Remember when Heather visited and we took her to Morro Bay and went to scouts there lol and got ramennnn. Just think this picture is so cute hehehe.",
+    lat: 35.366701,
+    lng: -120.84976,
+    photo: morrobay,
+  },
+  {
+    id: "15",
+    name: "Mumu Hot Pot",
+    caption:
+      "Remember when we went to Mumu hot pot with Justin and Marisa. It was sooo yummmy. Thank you for always taking me to eat yummy food bebe.",
+    lat: 37.406251,
+    lng: -121.995697,
+    photo: mumuhotpot,
+  },
+  {
+    id: "16",
+    name: "The Valleyyy",
+    caption:
+      "Remember when we went my home and I got to take you to all my favorite places and get boba and watch the sunset. I'm so glad you came with me and we spent time there.",
+    lat: 34.287098,
+    lng: -118.537855,
+    photo: reseda,
+  },
+  {
+    id: "17",
+    name: "Santa Barbara in the gardennn",
+    caption:
+      "This is when we went to Santa Barbara and laid in the sun with the pretty gardennn. And then we looked at that museum and got Korean BBQ after. So yummy.",
+    lat: 34.437304,
+    lng: -119.711146,
+    photo: sb,
+  },
+  {
+    id: "18",
+    name: "Sensorio",
+    caption:
+      "So romantic when we went to go see the lights. I remember doing this right before you left SLO for the last time before Korea. I love taking you on dates like these.",
+    lat: 35.651562,
+    lng: -120.622398,
+    photo: sensorio,
+  },
+  {
+    id: "19",
+    name: "SF First time Together",
+    caption:
+      "So romantic when we went to go see the lights. I remember doing this right before you left SLO for the last time before Korea. I love taking you on dates like these.",
+    lat: 37.77493,
+    lng: -122.41942,
+    photo: sf,
+  },
+  {
+    id: "20",
+    name: "Sizzlin Gogi",
+    caption:
+      "When we went to Sizzlin Gogi for the first time together heheh. And thennn we went to Boomers after and I beat you at mini golf. I love that you are always down to go eat hehe.",
+    lat: 34.927591,
+    lng: -120.435834,
+    photo: sizzlingogi,
+  },
+  {
+    id: "21",
+    name: "SJ Valley Fair",
+    caption:
+      "The first time we went to Valley fair togetherrr. I love when you take me to places I have never been to for the first time. I got you the bracelet and it makes me happy that you still wear it. I love you.",
+    lat: 37.325327,
+    lng: -121.944841,
+    photo: sj,
+  },
+  {
+    id: "22",
+    name: "Water Lantern!!",
+    caption:
+      "One of my favorite memories is relaxing on the lawn and doing the water lanterns with you. This was so romantic and so cute and I can't wait to do it again!! Need to bring more snacks next time.",
+    lat: 33.727586,
+    lng: -117.910353,
+    photo: waterlantern,
   },
 ];
 
